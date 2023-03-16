eb2cf0eeaf555ad2384c0580dac70a0964cc19a6
145849 <1485849@senati.pe>
2023-03-16 09:20:19 -0500

    actualizacion guardada

--- jueves 16_03_23/mayor_menor_que_10/src/mayor_menor_que_10/Mayor_menor_que_10.java
+++ jueves 16_03_23/mayor_menor_que_10/src/mayor_menor_que_10/Mayor_menor_que_10.java
@@ -17,7 +17,9 @@ public class Mayor_menor_que_10 {
         // TODO code application logic here
         Scanner lector=new Scanner(System.in):
         System.out.println("ingrese el numero: ");
-        
+        int n=lector.nextInt();
+        int [] Arreglo=new int [n];
+        for(int i =10;i<n;1++)
     }
     
 }
