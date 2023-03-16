9a4aec64d0af050293a37d7096a65fd4b904a0cf
145849 <1485849@senati.pe>
2023-03-16 09:56:51 -0500

    nuevo actualizacion

--- jueves 16_03_23/mayor_menor_que_10/src/mayor_menor_que_10/Mayor_menor_que_10.java
+++ jueves 16_03_23/mayor_menor_que_10/src/mayor_menor_que_10/Mayor_menor_que_10.java
@@ -9,17 +9,32 @@ package mayor_menor_que_10;
  * @author Alumno
  */
 public class Mayor_menor_que_10 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner lector=new Scanner(System.in):
+        Scanner lector=new Scanner(System.in;
         System.out.println("ingrese el numero: ");
         int n=lector.nextInt();
         int [] Arreglo=new int [n];
-        for(int i =10;i<n;1++)
-    }
-    
+        for(int i=0;i<n;i++)
+        {
+            Arreglo[i]=lector.nextInt();
+        }      
+        int menor,mayor;
+        menor=mayor=Arreglo[0];
+        for(int j=1;j<n;j++)
+        {
+            if(Arreglo[j]>mayor)
+            {
+                mayor=Arreglo[j];
+            }
+            if(Arreglo[j]<menor)
+            {
+                menor=Arreglo[j];
+            }
+        }
+        System.out.println("el numero mayor es : ");
+        System.out.println(mayor);
+        System.out.println("El mayor menor es: ");
+        System.out.println(menor);
+    } 
+     
 }
