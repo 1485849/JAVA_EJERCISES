5d5915537a0cc143660ca0ac2d32ab9d598980b0
145849 <1485849@senati.pe>
2023-03-17 10:44:39 -0500

    indicar si es numero positivo o negativo

---
+++ viernes 17_03_23/positive_or_negative_number/src/positive_or_negative_number/Positive_or_negative_number.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package positive_or_negative_number;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Positive_or_negative_number {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
