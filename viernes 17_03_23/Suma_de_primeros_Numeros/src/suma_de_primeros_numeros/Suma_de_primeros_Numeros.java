c6d374dbac48713fc8f4501e0c867e489c91bfab
145849 <1485849@senati.pe>
2023-03-17 11:18:01 -0500

    suma de los primeros numeros actualizado

---
+++ viernes 17_03_23/Suma_de_primeros_Numeros/src/suma_de_primeros_numeros/Suma_de_primeros_Numeros.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package suma_de_primeros_numeros;
+import java.util.*;
+/**
+ *
+ * @author Alumno
+ */
+public class Suma_de_primeros_Numeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner name = new Scanner(System.in);
+        System.out.println("ingrese su nombre");
+        String nombre = name.nextLine();
+      
+    
+}
