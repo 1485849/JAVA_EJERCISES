de443d7513840e1871006098e0cf30efde6952fa
145849 <1485849@senati.pe>
2023-03-18 07:36:07 -0500

    intento 2 de suma de numeros consecutivos

---
+++ viernes 17_03_23/suma_de_numeros_consecutivos_2/src/suma_de_numeros_consecutivos_2/Suma_de_numeros_consecutivos_2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package suma_de_numeros_consecutivos_2;
+import java.util.*;
+/**
+ *
+ * @author Alumno
+ */
+public class Suma_de_numeros_consecutivos_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    using namespace std;
+    main(){
+        int n,s=0;
+        cout<<"ingrese el numero: ";cin>>n;
+        for (int i=1;i<=n;i++){
+            
+        }
+              
+        
+    }
+    }
+    
+}
