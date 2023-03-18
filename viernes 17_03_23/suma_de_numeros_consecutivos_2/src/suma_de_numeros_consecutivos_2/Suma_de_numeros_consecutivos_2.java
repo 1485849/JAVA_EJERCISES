98f1de75b5c57cf76b2c0895164d06b24f1f5f75
145849 <1485849@senati.pe>
2023-03-18 08:14:12 -0500

    la suma de los numeros consecutivos 2

--- viernes 17_03_23/suma_de_numeros_consecutivos_2/src/suma_de_numeros_consecutivos_2/Suma_de_numeros_consecutivos_2.java
+++ viernes 17_03_23/suma_de_numeros_consecutivos_2/src/suma_de_numeros_consecutivos_2/Suma_de_numeros_consecutivos_2.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package suma_de_numeros_consecutivos_2;
-import java.util.*;
+import java.util.Scanner;
 /**
  *
  * @author Alumno
@@ -15,16 +15,12 @@ public class Suma_de_numeros_consecutivos_2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    using namespace std;
-    main(){
-        int n,s=0;
-        cout<<"ingrese el numero: ";cin>>n;
-        for (int i=1;i<=n;i++){
-            
-        }
-              
-        
-    }
+         Scanner name = new Scanner(System.in);
+        System.out.println("ingrese su nombre");
+        String nombre = name.nextLine();
+        System.out.println("Hello "+nombre+ "!");
+         //*****************************************
+
     }
     
 }
