4977438a39d01cf5d6ffb2ac82c33fc8517ece02
1485849 <1485849@senati.pe>
2023-03-24 11:06:14 -0500

    actualizacion de diferencia mayor y  menor

---
+++ viernes 29_03_23/diferencia _mayor_menor/src/diferencia/_mayor_menor/Diferencia_mayor_menor.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package diferencia._mayor_menor;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Diferencia_mayor_menor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero;
+        
+       System.out.println("por favor ingrese el primer numero");
+       System.out.println("ingrese el segundo numero");
+    }
+    
+}
