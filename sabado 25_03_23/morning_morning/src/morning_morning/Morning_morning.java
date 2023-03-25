f0b0d6c1b90645952fba63e8ba53da4b45810b4c
1485849 <1485849@senati.pe>
2023-03-25 08:22:17 -0500

    morning, morning exercises

---
+++ sabado 25_03_23/morning_morning/src/morning_morning/Morning_morning.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package morning_morning;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Morning_morning {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println(" morning, morning,morning ");
+        System.out.println("           hello, Have a good day");
+        System.out.println("    j   a   a     v   v    a   a");
+        System.out.println(" j  j  aaaaaaa     v v    aaaaaaa");
+        System.out.println("  jj  a       a     v    a       a");
+    }
+    
+}
