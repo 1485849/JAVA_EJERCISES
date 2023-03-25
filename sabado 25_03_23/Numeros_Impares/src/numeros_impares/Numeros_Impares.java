ebffe0b19accf2fb77f3cf76fd261ccd96dd69c8
1485849 <1485849@senati.pe>
2023-03-25 10:00:57 -0500

    numeros impares entre el rango 1 al 999 actualizado

--- sabado 25_03_23/Numeros_Impares/src/numeros_impares/Numeros_Impares.java
+++ sabado 25_03_23/Numeros_Impares/src/numeros_impares/Numeros_Impares.java
@@ -3,27 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package numeros_impares;
-import java.util.Scanner;
-/**
- *
- * @author Alumno
- */
+
 public class Numeros_Impares {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner leer = new Scanner (System.in);
-        System.out.println("los numeros pares");
-        int ra,r;
-        for (ra=1;ra<999;ra++);{
-             r= ra % 2;
-             if (r == 0){
-                 System.out.println("");
-            }
-            }}
+    System.out.println("los numeros impares entre el 1 y el 999 son");
+    for(int i=1; i<=999; i++){
+        if (i % 2 ==1)
+            System.out.println(""+i);
+    }
+        
+    
+    }
     
     
 }
